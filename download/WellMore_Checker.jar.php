148e55c9257c6878ee266df02821649e113ac212
WellMore <44681085+WellMore@users.noreply.github.com>
2019-07-02 14:32:50 +0300

    Add files via upload

---
+++ download/WellMore_Checker.jar.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    if(isset($_REQUEST["file"])){
+        // Get parameters
+        $file = urldecode($_REQUEST["file"]); // Decode URL-encoded string
+        $filepath = "images/" . $file;
+
+        // Process download
+        if(file_exists($filepath)) {
+            header('Content-Description: File Transfer');
+            header('Content-Type: application/octet-stream');
+            header('Content-Disposition: attachment; filename="'.basename($filepath).'"');
+            header('Expires: 0');
+            header('Cache-Control: must-revalidate');
+            header('Pragma: public');
+            header('Content-Length: ' . filesize($filepath));
+            flush(); // Flush system output buffer
+            readfile($filepath);
+            exit;
+        }
+    }
+    ?>
+  </body>
+</html>
